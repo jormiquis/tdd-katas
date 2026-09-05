0ea503225597ed036d8d0c41d027216adc55122a
jormiquis <193163902+jormiquis@users.noreply.github.com>
2026-09-05 14:00:55 +0200

    refactor: Use PascalCase for test class
    Signed-off-by: jormiquis <193163902+jormiquis@users.noreply.github.com>

--- src/guessTheNumber.test.ts
+++ src/guessTheNumber.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest';
 import { GuessingNumberGame } from './guessTheNumber';
 import { RandomNumberGenerator } from './RandomNumberGenerator';
 
-class testRandomNumberGenerator implements RandomNumberGenerator {
+class TestRandomNumberGenerator implements RandomNumberGenerator {
   generate(): number {
     return 6;
   }
@@ -11,7 +11,7 @@ class testRandomNumberGenerator implements RandomNumberGenerator {
 
 describe('Guess the number', () => {
   it('should print success message when guessed number matches generated one', () => {
-    const generator : testRandomNumberGenerator = new testRandomNumberGenerator();
+    const generator : TestRandomNumberGenerator = new TestRandomNumberGenerator();
 
     const game: GuessingNumberGame = new GuessingNumberGame(generator);
     const playString: string = game.guessNumber(6);
